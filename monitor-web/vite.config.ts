6f0ffa87045686baf276b2484bf7544a927c1802
mjlee <yenchieh0826@gmail.com>
2025-11-14 22:11:38 +0800

    port

--- monitor-web/vite.config.ts
+++ monitor-web/vite.config.ts
@@ -4,4 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),],
+  server: {
+    port: 3000, // 這裡改成你想要的 port
+  },
 })
