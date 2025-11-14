30600bd1554da6c739280a739fabd3c1aa58a60b
mjlee <yenchieh0826@gmail.com>
2025-11-14 22:31:51 +0800

    port

--- monitor-web/src/consts.ts
+++ monitor-web/src/consts.ts
@@ -1,6 +1,6 @@
-export const SCHEDULER_URL = 'http://localhost:22225/';
-// export const SCHEDULER_URL = 'http://ws2.csie.ntu.edu.tw:22225/';
+// export const SCHEDULER_URL = 'http://localhost:22225/';
+export const SCHEDULER_URL = 'http://ws2.csie.ntu.edu.tw:22225/';
 export const SCHEDULER_HEALTH_ENDPOINT = '/status'
 
-// export const TYPESENSE_URL = 'http://ws2.csie.ntu.edu.tw:22222/'
-export const TYPESENSE_URL = 'http://localhost:22222/'
+export const TYPESENSE_URL = 'http://ws2.csie.ntu.edu.tw:22222/'
+// export const TYPESENSE_URL = 'http://localhost:22222/'
